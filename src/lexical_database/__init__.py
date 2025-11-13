c3fea428764393df01855d3aaca94ec6e3ba09c1
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2025-11-13 20:21:08 +0300

    Move logger setup to a separate module

--- src/lexical_database/__init__.py
+++ src/lexical_database/__init__.py
@@ -4,41 +4,9 @@ from model.word import Word
 from .corpus_word import make_corpus_word, word_to_corpus_word
 from morph import Morph, MultiMorph
 from re import compile
-from os import makedirs
-from os.path import join
-from logging import getLogger, FileHandler, DEBUG, Formatter, LogRecord
-from contextvars import ContextVar
-
-ctx_text_path: ContextVar[str] = ContextVar('text_path')
-ctx_text_path.set('Unknown directory')
-
-ctx_text_id: ContextVar[str] = ContextVar('text_id')
-ctx_text_id.set('Unknown directory')
-
-ctx_line_id: ContextVar[str] = ContextVar('line_id')
-ctx_line_id.set('Unknown line')
-
-ctx_word_tag: ContextVar[str] = ContextVar('word_tag')
-ctx_word_tag.set('Unknown word')
-
-def log_filter(record: LogRecord) -> LogRecord:
-  record.text_path = ctx_text_path.get()
-  record.text_id = ctx_text_id.get()
-  record.line_id = ctx_line_id.get()
-  record.word_tag = ctx_word_tag.get()
-  return record
-
-makedirs('logs', exist_ok=True)
-for package in ['model.line', 'model.word', 'model.selection', 'morph',
-                'lexical_database', 'lexical_database.corpus_word']:
-  handler = FileHandler(join('logs', f'{package}.log'), 'w', encoding='utf-8')
-  handler.setLevel(DEBUG)
-  formatter = Formatter('%(text_path)s\n%(text_id)s\n%(line_id)s\n%(word_tag)s\n%(levelname)s: %(message)s\n')
-  handler.setFormatter(formatter)
-  handler.addFilter(log_filter)
-  logger = getLogger(package)
-  logger.setLevel(DEBUG)
-  logger.addHandler(handler)
+from .loggers import ctx_text_path, ctx_text_id, ctx_line_id, ctx_word_tag
+from logging import getLogger
+logger = getLogger(__name__)
 
 fragmentary_form_indicators = {'[', ']', 'x'}
 
@@ -69,7 +37,6 @@ class LexicalDatabase:
     self.glosses = defaultdict[str, set[str]](set)
     self.concordance = defaultdict[str, set[str]](set)
     self.corpus = dict[str, list[dict[str, str]]]()
-    self.logger = getLogger(__name__)
 
   """Add the words occuring in a translation to the
   list possible tranlsations for a particular stem
@@ -118,13 +85,15 @@ class LexicalDatabase:
                       self.update_glosses(analysis)
                       self.concordance[analysis_str].add(attestation)
                   else:
-                    self.logger.error('The selected morphological analysis number %i is not available.', number)
+                    logger.error(
+                      'The selected morphological analysis number %i is not available.', number
+                    )
             corpus_line.append(word_to_corpus_word(word))
           except (KeyError, ValueError):
             msg = 'Cannot parse word:\n{0}\non line {1} in {2}'.format(
               str(tag), line.line_id, line.text_path
             )
-            self.logger.exception(msg)
+            logger.exception(msg)
         else:
           corpus_line.append(make_corpus_word(str(tag)))
       if (len(corpus_line) > 0):
