d3a524a106adf620a87faf60fc83f6bc816137b8
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2025-11-17 17:51:58 +0300

    Add enclitic chain analyses to Hittite words

--- src/lexical_database/analysis.py
+++ src/lexical_database/analysis.py
@@ -10,6 +10,10 @@ def make_analysis(selection: Selection, morph: Morph) -> str:
     morph_tag = morph.get_morph_tag(selection.gramm_form)
     if morph_tag is None:
       morph_tag = ERROR_SYMBOL
+    if morph.enclitics_analysis is not None:
+      enclitic_chain = morph.enclitics_analysis.get_morph_tag(selection.encl_chain)
+      if enclitic_chain is not None:
+        morph_tag += '=' + enclitic_chain
     if morph_tag == '':
       return morph.translation
     elif morph_tag.startswith('.') or morph_tag.startswith('='):
