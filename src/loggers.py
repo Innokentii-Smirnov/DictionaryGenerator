bda0a935167243e7d84ae3d24c28e45bf56f5107
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2025-11-17 21:00:29 +0300

    Create a directory for logs before adding log handlers

--- src/loggers.py
+++ src/loggers.py
@@ -1,10 +1,14 @@
+import os
 from os.path import join
 from logging import getLogger, FileHandler, DEBUG, Formatter, LogRecord
 from model.corpus import PROCESSED_FILE_LOGGER_NAME, SKIPPED_FILE_LOGGER_NAME
+LOGGER_DIR = 'logs'
 logger_names = [PROCESSED_FILE_LOGGER_NAME, SKIPPED_FILE_LOGGER_NAME, '__main__', 'model.corpus', 'model.text']
 
+os.makedirs(LOGGER_DIR, exist_ok=True)
+
 for logger_name in logger_names:
-  handler = FileHandler(join('logs', f'{logger_name}.log'), 'w', encoding='utf-8')
+  handler = FileHandler(join(LOGGER_DIR, f'{logger_name}.log'), 'w', encoding='utf-8')
   handler.setLevel(DEBUG)
   formatter = Formatter('%(message)s')
   handler.setFormatter(formatter)
