ca0b99f1a8ad3a650349dcbd35f8a4396eefd62f
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-03-24 00:17:53 +0100

    Use the morphemic converter

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 python src/gen_dict_from_conllu.py \
   ~/OldNubianUDTreebank/OldNubian.conllu \
   ~/OldNubianUDTreebank/MorphemicLinearizer.txt \
-  ~/OldNubianUDTreebank/Converter.txt \
+  ~/OldNubianUDTreebank/MorphemicConverter.txt \
   ~/OldNubianDictionary/OldNubianDictionary.json
 cat logs/__main__.log
