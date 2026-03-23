7b1e19736eb388a034f9a4a04dce61c3a5846aa2
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-03-23 22:03:18 +0100

    Modify the run script

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 python src/gen_dict_from_conllu.py \
   ~/OldNubianUDTreebank/OldNubian.conllu \
-  ~/OldNubianUDTreebank/Linearizer.txt \
+  ~/OldNubianUDTreebank/MorphemicLinearizer.txt \
   ~/OldNubianUDTreebank/Converter.txt \
-  ~/OldNubianUDTreebank/OldNubianDictionary.json
+  ~/OldNubianDictionary/OldNubianDictionary.json
 cat logs/__main__.log
